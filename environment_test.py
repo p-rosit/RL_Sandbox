c4e92dfb4355882504791bc76436892ab1868ca7
p-rosit <pontus.rosqvist@gmail.com>
2023-02-19 13:44:25 +0100

    action replay buffer

--- environment_test.py
+++ environment_test.py
@@ -1,4 +1,23 @@
-import gym
+import gymnasium as gym
+from experience_replay_buffer import ReplayBuffer
+
+
+def main():
+    env = gym.make("LunarLander-v2", render_mode="human")
+    buffer = ReplayBuffer(max_size=100)
+
+    prev_observation, info = env.reset(seed=42)
+    for _ in range(1000):
+        action = env.action_space.sample()  # this is where you would insert your policy
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        buffer.append(prev_observation, action, reward, observation)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+        prev_observation = observation
+    env.close()
+
 
 if __name__ == '__main__':
-	print(':)')
+    main()
---
+++ experience_replay_buffer.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+rng = np.random.default_rng()
+
+class ReplayBuffer:
+    def __init__(self, max_size=1000):
+        self.ind = 0
+        self.max_size = max_size
+        self.buffer = []
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, state, action, reward, next_state):
+        experience = (state, action, reward, next_state)
+
+        if len(self.buffer) < self.max_size:
+            self.buffer.append(experience)
+        else:
+            self.buffer[self.ind] = experience
+
+        self.ind = (self.ind + 1) % self.max_size
+
+    def sample(self, batch_size=1):
+        return [self.buffer[ind] for ind in rng.choice(len(self.buffer), batch_size)]
+
+if __name__ == '__main__':
+    b = ReplayBuffer(max_size=2)
+
+    b.append([1, 2], 2, 3, [4, 5])
+    b.append([2, 3], 3, 4, [5, 6])
+    b.append([3, 4], 4, 5, [6, 7])
+
+    print(b.buffer)
+    print(b.sample(2))
