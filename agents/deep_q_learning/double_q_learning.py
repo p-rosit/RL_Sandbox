d5839cc136a69b5a39f001e7b37d6d92bf4136a2
p-rosit <pontus.rosqvist@gmail.com>
2023-02-26 12:13:15 +0100

    fixed modified double q learning and clipped double q learning

--- agents/deep_q_learning/double_q_learning.py
+++ agents/deep_q_learning/double_q_learning.py
@@ -71,7 +71,6 @@ class DoubleQLearningAgent(AbstractAgent):
         experiences = (states, actions, rewards, non_final_next_states, non_final_mask)
 
         loss = torch.tensor([0], dtype=torch.float64)
-
         loss += self._compute_loss(self.policy_network_1, self.target_network_2, experiences)
         loss += self._compute_loss(self.policy_network_2, self.target_network_1, experiences)
 
@@ -145,4 +144,71 @@ class ModifiedDoubleQLearningAgent(AbstractAgent):
 
 class ClippedDoubleQLearning(AbstractAgent):
     def __init__(self, input_size, layer_sizes, output_size, discount=0.99, tau=0.005, max_grad=100):
-        pass
+        super().__init__()
+        layers = (input_size, *layer_sizes, output_size)
+
+        network_1 = []
+        network_2 = []
+        for size_in, size_out in zip(layers[:-1], layers[1:]):
+            network_1.append(nn.Linear(size_in, size_out))
+            network_1.append(nn.ReLU())
+            network_2.append(nn.Linear(size_in, size_out))
+            network_2.append(nn.ReLU())
+        network_1.pop()
+        network_2.pop()
+
+        self.discount = discount
+        self.policy_network_1 = nn.Sequential(*network_1)
+        self.policy_network_2 = nn.Sequential(*network_2)
+        self.target_network_1 = SoftUpdateModel(self.policy_network_1, tau=tau)
+        self.target_network_2 = SoftUpdateModel(self.policy_network_2, tau=tau)
+        self.max_grad = max_grad
+
+    def sample(self, state):
+        if self.training:
+            if torch.rand(1) < 0.5:
+                return self.policy_network_1(state).argmax(dim=1).view(1, 1)
+            else:
+                return self.policy_network_2(state).argmax(dim=1).view(1, 1)
+        else:
+            value_1, action_1 = self.policy_network_1(state).max(dim=1)
+            value_2, action_2 = self.policy_network_2(state).max(dim=1)
+            if value_1 > value_2:
+                return action_1
+            else:
+                return action_2
+
+    def parameters(self):
+        return *self.policy_network_1.parameters(), *self.policy_network_2.parameters()
+
+    def step(self, experiences):
+        batch_experiences = Transition(*zip(*experiences))
+
+        states = torch.cat(batch_experiences.state, dim=0)
+        actions = torch.cat(batch_experiences.action, dim=0)
+        rewards = torch.cat(batch_experiences.reward, dim=0)
+
+        non_final_mask = torch.tensor([next_state is not None for next_state in batch_experiences.next_state], dtype=torch.bool)
+        non_final_next_states = torch.cat([next_state for next_state in batch_experiences.next_state if next_state is not None])
+
+        estimated_action_values_1 = self.policy_network_1(states).gather(1, actions).squeeze()
+        estimated_action_values_2 = self.policy_network_2(states).gather(1, actions).squeeze()
+
+        estimated_next_action_values = torch.zeros_like(rewards)
+        with torch.no_grad():
+            estimated_next_values_1, _ = self.target_network_1(non_final_next_states).max(dim=1)
+            estimated_next_values_2, _ = self.target_network_2(non_final_next_states).max(dim=1)
+            estimated_next_action_values[non_final_mask] = torch.min(estimated_next_values_1, estimated_next_values_2)
+
+        bellman_action_values = rewards + self.discount * estimated_next_action_values
+
+        loss_1 = self.criterion(estimated_action_values_1, bellman_action_values)
+        loss_2 = self.criterion(estimated_action_values_2, bellman_action_values)
+
+        self.optimizer.zero_grad()
+        (loss_1 + loss_2).backward()
+        torch.nn.utils.clip_grad_value_(self.parameters(), self.max_grad)
+        self.optimizer.step()
+
+        self.target_network_1.update(self.policy_network_1)
+        self.target_network_2.update(self.policy_network_2)
--- q_learning_environment.py
+++ q_learning_environment.py
@@ -7,7 +7,7 @@ from buffer.experience_replay_buffer import ReplayBuffer
 
 from agents.random_agent import RandomAgent
 from agents.deep_q_learning.q_learning import DenseQLearningAgent
-from agents.deep_q_learning.double_q_learning import DoubleQLearningAgent, ModifiedDoubleQLearningAgent
+from agents.deep_q_learning.double_q_learning import DoubleQLearningAgent, ModifiedDoubleQLearningAgent, ClippedDoubleQLearning
 from core.agent_wrappers import AnnealAgent
 
 import matplotlib.pyplot as plt
@@ -31,7 +31,8 @@ def main():
 
     # q = DenseQLearningAgent(4, [128, 128], 2, discount=gamma, tau=tau)
     # q = DoubleQLearningAgent(4, [128, 128], 2, discount=gamma, tau=tau)
-    q = ModifiedDoubleQLearningAgent(4, [128, 128], 2, discount=gamma, tau=tau)
+    # q = ModifiedDoubleQLearningAgent(4, [128, 128], 2, discount=gamma, tau=tau)
+    q = ClippedDoubleQLearning(4, [128, 128], 2, discount=gamma, tau=tau)
     q.set_criterion(nn.SmoothL1Loss())
     q.set_optimizer(optim.AdamW(q.parameters(), lr=lr, amsgrad=True))
 
