2f7512077e6a4fe5a5b67a9a9f1ce08e1944981a
p-rosit <pontus.rosqvist@gmail.com>
2023-03-01 17:17:42 +0100

    started actor critic implementation

---
+++ core/abstract_actor_critic_agent.py
@@ -0,0 +1,21 @@
+import torch
+from core.abstract_agent import AbstractAgent
+
+class AbstractActorCritic(AbstractAgent):
+    def __init__(self):
+        super().__init__()
+
+    def sample(self, state):
+        raise NotImplementedError
+
+    def parameters(self):
+        raise NotImplementedError
+
+    def _step(self, loss):
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_value_(self.parameters(), self.max_grad)
+        self.optimizer.step()
+
+    def step(self, experiences):
+        raise NotImplementedError
