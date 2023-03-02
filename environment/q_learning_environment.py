df67a133ee8a527f6f8c255b92d67fa05d6822cc
p-rosit <pontus.rosqvist@gmail.com>
2023-03-02 08:40:48 +0100

    moved training to class and added validation

---
+++ environment/q_learning_environment.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import torch
+
+class QLearningEnvironment:
+    def __init__(self, env, buffer):
+        self.env = env
+        self.buffer = buffer
+
+    def train(self, agent, num_episodes, batch_size, eval_episodes=0, plot=False):
+        episode_reward = []
+        evaluation_episode = []
+        evaluation_reward = []
+        fig = plt.figure(1) if plot else None
+        ax = fig.subplots() if fig is not None else None
+
+        for ep in range(num_episodes):
+            if ep % 10 == 0 and eval_episodes > 0:
+                agent.eval()
+                evaluation_episode.append(ep)
+                evaluation_reward.append(self.eval(agent, eval_episodes))
+                agent.train()
+
+            done = False
+            curr_reward = 0
+            state, info = self.env.reset()
+            state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+
+            while not done:
+                policy_action, env_action = agent.sample(state)
+                observation, reward, terminated, truncated, _ = self.env.step(env_action)
+                curr_reward += reward
+                reward = torch.tensor([reward])
+                done = terminated or truncated
+
+                if terminated:
+                    next_state = None
+                else:
+                    next_state = torch.tensor(observation, dtype=torch.float32).unsqueeze(0)
+
+                self.buffer.append(state, policy_action, reward, next_state)
+                state = next_state
+
+                if len(self.buffer) > batch_size:
+                    agent.step(self.buffer.sample(batch_size))
+
+            episode_reward.append(curr_reward)
+
+            if plot:
+                ax.cla()
+                ax.plot(episode_reward)
+                ax.plot(evaluation_episode, evaluation_reward)
+                plt.draw()
+                plt.pause(0.0001)
+
+    def eval(self, agent, num_episodes):
+        mean_reward = 0
+        for _ in range(num_episodes):
+            done = False
+            observation, info = self.env.reset()
+
+            while not done:
+                state = torch.tensor(observation, dtype=torch.float32).unsqueeze(0)
+                _, env_action = agent.sample(state)
+                observation, reward, terminated, truncated, _ = self.env.step(env_action)
+                mean_reward += reward
+                done = terminated or truncated
+
+        return mean_reward / num_episodes
--- q_learning_environment.py
+++ q_learning_environment.py
@@ -4,6 +4,7 @@ from torch import optim
 import gymnasium as gym
 
 from buffer.experience_replay_buffer import ReplayBuffer
+from environment.q_learning_environment import QLearningEnvironment
 
 from networks.DenseNetwork import DenseNetwork
 
@@ -13,13 +14,12 @@ from agents.off_policy.deep_q_learning.double_q_learning import DoubleQLearningA
 from agents.off_policy.deep_q_learning.multi_q_learning import MultiQLearningAgent
 from core.agent_wrappers import AnnealAgent
 
-import matplotlib.pyplot as plt
-
 def main():
     # env = gym.make("LunarLander-v2", render_mode="human")
     # env = gym.make("LunarLander-v2")
     env = gym.make("CartPole-v1")
     buffer = ReplayBuffer(max_size=50000)
+    environment = QLearningEnvironment(env, buffer)
 
     batch_size = 256
     gamma = 0.99
@@ -38,80 +38,17 @@ def main():
     net_3 = DenseNetwork(4, [128, 128], 2)
     net_4 = DenseNetwork(4, [128, 128], 2)
 
-    # q = QLearningAgent(net_1, discount=gamma, tau=tau)
+    q = QLearningAgent(net_1, discount=gamma, tau=tau)
     # q = DoubleQLearningAgent(net_1, net_2, discount=gamma, tau=tau, policy_train=False)
     # q = ModifiedDoubleQLearningAgent(net_1, discount=gamma, tau=tau)
-    q = ClippedDoubleQLearning(net_1, net_2, discount=gamma, tau=tau)
+    # q = ClippedDoubleQLearning(net_1, net_2, discount=gamma, tau=tau)
     # q = MultiQLearningAgent(net_1, net_2, net_3, net_4, discount=gamma, tau=tau, policy_train=False)
     q.set_criterion(nn.SmoothL1Loss())
     q.set_optimizer(optim.AdamW(q.parameters(), lr=lr, amsgrad=True))
 
     sq = AnnealAgent(q, r, start_steps=start_steps, eps_start=eps_start, eps_end=eps_end, decay_steps=eps_decay)
 
-    episode_reward = []
-    evaluation_episode = []
-    evaluation_reward = []
-    fig = plt.figure(1)
-    ax = fig.subplots()
-
-    for ep in range(num_episodes):
-        done = False
-        curr_reward = 0
-        state, info = env.reset()
-
-        if ep % 10 == 0:
-            # Run evaluation episode
-            sq.eval()
-            while not done:
-                state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-                _, env_action = sq.sample(state)
-                observation, reward, terminated, truncated, _ = env.step(env_action)
-                curr_reward += reward
-                done = terminated or truncated
-
-                if done:
-                    break
-
-                state = observation
-
-            evaluation_episode.append(ep)
-            evaluation_reward.append(curr_reward)
-
-            sq.train()
-            done = False
-            curr_reward = 0
-            state, info = env.reset()
-
-        state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-
-        while not done:
-            policy_action, env_action = sq.sample(state)
-            observation, reward, terminated, truncated, _ = env.step(env_action)
-            curr_reward += reward
-            reward = torch.tensor([reward])
-            done = terminated or truncated
-
-            if terminated:
-                next_state = None
-            else:
-                next_state = torch.tensor(observation, dtype=torch.float32).unsqueeze(0)
-
-            buffer.append(state, policy_action, reward, next_state)
-            state = next_state
-
-            if len(buffer) > batch_size:
-                sq.step(buffer.sample(batch_size))
-
-            if done:
-                break
-
-        episode_reward.append(curr_reward)
-
-        ax.cla()
-        ax.plot(episode_reward)
-        ax.plot(evaluation_episode, evaluation_reward)
-        plt.draw()
-        plt.pause(0.0001)
+    environment.train(sq, num_episodes, batch_size, eval_episodes=1, plot=True)
 
     env.close()
 
