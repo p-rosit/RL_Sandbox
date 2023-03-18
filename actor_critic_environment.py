6db814db4315e5d68efa2c849aeaf5dd356a4f0a
p-rosit <pontus.rosqvist@gmail.com>
2023-03-18 16:56:43 +0100

    testing ac

--- actor_critic_environment.py
+++ actor_critic_environment.py
@@ -43,7 +43,7 @@ def main():
     # environment.explore(RandomAgent(env), initial_episodes)
     # environment.pretrain(ac, optimizer, epochs, pre_batch, plot=True)
     # environment.buffer.clear()
-    environment.train(ac, optimizer, num_rollouts, train_steps=1, episodes_per_rollout=16, eval_episodes=10, td_steps=1, plot=True)
+    environment.train(ac, optimizer, num_rollouts, train_steps=5, episodes_per_rollout=8, eval_episodes=10, td_steps=3, plot=True)
 
     env.close()
 
